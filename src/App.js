1050040ad8f0f0ad55b713e494884b975d6f81e4
Mateo Komljenovic <mateo.komljenovic@chanalyze.com>
2022-09-24 17:28:55 +0200

    no one writes React like this

---
+++ src/App.js
@@ -0,0 +1,31 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ])
+}
+
+
+const App = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Iva",
+            animal: "Bunny",
+            breed: "Mini French Lop"
+        }),
+        React.createElement(Pet, {
+            name: "Zoki",
+            animal: "Cat",
+            breed: "European Common"
+        }),
+        React.createElement(Pet, {
+            name: "Ludomir",
+            animal: "Dog",
+            breed: "Mix"
+        }),
+    ])
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
