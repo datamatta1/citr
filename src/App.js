53f0e3888f74c7021897a727ae39e2c6ab557a6b
Mateo Komljenovic <mateo.komljenovic@chanalyze.com>
2022-09-26 18:53:22 +0200

    add strict mode for future proof

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { StrictMode } from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
 
