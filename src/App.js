e250c69052db4bdb3b51a66506ddcb7589c1b4ac
Mateo Komljenovic <mateo.komljenovic@chanalyze.com>
2022-09-26 19:49:14 +0200

    react-router implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
+      <BrowserRouter>
         <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>Hey oh!</h2>;
+};
+
+export default Details;
